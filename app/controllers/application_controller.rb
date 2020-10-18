4fe4c9ef274178e6769755941ada9345b28a1c12
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-18 15:19:47 +0000

    square root fully functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,24 @@ class ApplicationController < ActionController::Base
     @square_of_num = @num ** 2
     render({ :template => "calculation_templates/square_results.html.erb"})
   end
+
+  def blank_square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+  end
+
+  def calculate_square_root
+    # params = {"user_number"=>"44"}
+
+    @user_num = params.fetch("user_number").to_f
+    @square_root_of_num = @user_num ** (0.5)
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
+  end
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/rand_results.html.erb"})
+  end
 end
